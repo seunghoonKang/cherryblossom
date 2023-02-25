e33cc629112c1bbbdbe0a6370412e5ce380131f6
seonghun <pch468@naver.com>
2023-02-25 22:17:51 +0900

    feat: set textarea not to resize, add placehoder

--- src/components/Creation/Display/index.tsx
+++ src/components/Creation/Display/index.tsx
@@ -1,7 +1,7 @@
-export default function Display() {
+export default function Display() {  
   return <div className="w-full flex justify-center">
     <div className="w-[320px] h-[300px] border border-solid rounded-lg border-fuchsia-300 flex justify-center items-center">
-      <textarea className="w-[220px] h-[140px]">
+      <textarea className="w-[220px] h-[140px] p-1 resize-none " placeholder="초대장 문구를 작성해주세요">
       </textarea>
     </div>
   </div>;
