bc1a3b602bff6e8e397c045e5c9f2d52e6eba2dd
seunghoon <tmdgns4321@gmail.com>
2023-02-25 17:23:12 +0900

    fix: intro button fix
    버튼 onClick 이벤트 리뷰 반영

--- pages/intro/index.tsx
+++ pages/intro/index.tsx
@@ -17,7 +17,7 @@ function Intro() {
           친구랑 벚꽃 보러 갈까요?
         </p>
       </div>
-      <button className="rounded-3xl bg-zinc-300 mt-4 w-80 h-12" onClick={() => movePage()}>
+      <button className="rounded-3xl bg-zinc-300 mt-4 w-80 h-12" onClick={movePage}>
         초대장 만들러 가기
       </button>
     </div>
