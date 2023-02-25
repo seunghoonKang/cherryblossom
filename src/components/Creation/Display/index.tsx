7bfa17981fea7f263047049683d90a5d8e17d80f
seonghun <pch468@naver.com>
2023-02-25 18:27:32 +0900

    feat : add display and textarea

--- src/components/Creation/Display/index.tsx
+++ src/components/Creation/Display/index.tsx
@@ -1,3 +1,8 @@
 export default function Display() {
-  return <div></div>;
+  return <div className="w-full flex justify-center">
+    <div className="w-[320px] h-[300px] border border-solid rounded-lg border-fuchsia-300 flex justify-center items-center">
+      <textarea className="w-[220px] h-[140px]">
+      </textarea>
+    </div>
+  </div>;
 }
