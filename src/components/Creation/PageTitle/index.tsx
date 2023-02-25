ddc7762f8ea506c638f166c2f6a4f0e88d610a01
Lee-Yeseul <ssulv3030@gmail.com>
2023-02-25 17:37:19 +0900

    feat : add pageTitle component

--- src/components/Creation/PageTitle/index.tsx
+++ src/components/Creation/PageTitle/index.tsx
@@ -1,3 +1,9 @@
 export default function PageTitle() {
-  return <div></div>;
+  return (
+    <div className="w-full flex justify-center">
+      <div className="w-60 bg-pink-200 text-center py-3 rounded-lg drop-shadow-md">
+        나만의 벚꽃 초대장
+      </div>
+    </div>
+  );
 }
