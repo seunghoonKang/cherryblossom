2e970839899c9a561a22e4c6ae4593e7690422e6
Lee-Yeseul <ssulv3030@gmail.com>
2023-02-26 17:35:24 +0900

    feat : img file filebase로 저장하는 함수 추가

--- pages/creation/index.tsx
+++ pages/creation/index.tsx
@@ -1,8 +1,10 @@
 import Custom from '@/src/components/Creation/Custom';
 import Display from '@/src/components/Creation/Display';
 import PageTitle from '@/src/components/Creation/PageTitle';
+import { saveImg } from '@/src/utils';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 /**
  * 초대장 생성 페이지
@@ -24,7 +26,7 @@ const Creation = () => {
   };
 
   return (
-    <>
+    <div id="temp">
       <PageTitle />
       <Display />
       <Custom
@@ -34,23 +36,16 @@ const Creation = () => {
         selectedItem={selectedItem}
         setSelectedItem={(item: CustomTypes) => setSelectedItem(item)}
       />
-      {isTextEmpty ? (
-        <button
-          // onClick={}
-          className="w-[360px] h-12 bg-blossom-green"
-        >
-          초대장 완성하기
-        </button>
-      ) : (
-        <button
-          // onClick={}
-          disabled
-          className="w-[360px] font-pretendard font-bold h-12 bg-blossom-gray"
-        >
-          초대장 완성하기
-        </button>
-      )}
-    </>
+      <button
+        disabled={isTextEmpty}
+        onClick={() => saveImg('temp', uuidv4())}
+        className={`w-[360px] font-pretendard font-bold h-12 ${
+          isTextEmpty ? 'bg-blossom-gray' : ' bg-blossom-green'
+        } bg-blossom-gray`}
+      >
+        초대장 완성하기
+      </button>
+    </div>
   );
 };
 
