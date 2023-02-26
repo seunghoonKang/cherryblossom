97dea5363aebfd3651a92f3be61c2e25e460195b
Lee-Yeseul <ssulv3030@gmail.com>
2023-02-26 22:31:39 +0900

    feat : custom file 추가 및 스타일 수정

--- src/components/Creation/Custom/index.tsx
+++ src/components/Creation/Custom/index.tsx
@@ -1,6 +1,11 @@
+import Image from 'next/image';
+
 type CustomTypes = 'background' | 'character' | 'sticker';
 
 type CustomProps = {
+  selectedBackground: number | null;
+  selectedCharacter: number | null;
+  selectedSticker: number | null;
   setSelectedBackground: (item: number | null) => void;
   setSelectedCharacter: (item: number | null) => void;
   setSelectedSticker: (item: number | null) => void;
@@ -19,35 +24,34 @@ const CUSTOM_ITEMS: CustomItem[] = [
   { name: '스티커', value: 'sticker' },
 ];
 
-const MOCK_IMAGES = [
+const BACKGROUND_IMAGE = [
   {
     id: 0,
-    value: 'url1',
+    preview: 'bg_preview_flower_way.svg',
+    value: 'bg_flower_way.svg',
   },
   {
     id: 1,
-    value: 'url2',
+    preview: 'bg_preview_yellow_flower_garden.svg',
+    value: 'bg_yellow_flower_garden.svg',
   },
-  {
-    id: 2,
-    value: 'url3',
-  },
-  {
-    id: 3,
-    value: 'url1',
-  },
-  {
-    id: 4,
-    value: 'url2',
-  },
-  {
-    id: 5,
-    value: 'url3',
-  },
+];
+
+const STICKER_IMAGE = [
+  { id: 0, preview: 'st_preview_cherry_blossom.svg', value: 'st_cherry_blossom.png' },
+  { id: 1, preview: 'st_preview_flower.svg', value: 'st_flower.png' },
+];
+
+const CHARACTER_IMAGE = [
+  { id: 0, preview: 'st_preview_cherry_blossom.svg', value: 'st_cherry_blossom.png' },
+  { id: 1, preview: 'st_preview_flower.svg', value: 'st_flower.png' },
 ];
 
 export default function Custom(props: CustomProps) {
   const {
+    selectedBackground,
+    selectedCharacter,
+    selectedSticker,
     setSelectedBackground,
     setSelectedCharacter,
     setSelectedSticker,
@@ -74,13 +78,13 @@ export default function Custom(props: CustomProps) {
   };
 
   return (
-    <div className="w-[360px] h-96 bg-blossom-lightBlue flex-col space-y-2">
-      <div className="grid grid-cols-3 gap-4 pl-5 pr-3 mx-1">
+    <div className="w-[360px] h-96 bg-blossom-lightBlue flex-col space-y-[8px] mt-[8px] px-[20px]">
+      <div className="grid grid-cols-3 gap-4">
         {CUSTOM_ITEMS.map(custom => {
           return (
             <button
               key={custom.value}
-              className={`w-24 h-9 text-sm rounded border-blossom-white border-2 
+              className={`w-[96px] h-[36px] text-sm rounded-[10px] border-blossom-white border-2 
   ${selectedItem === custom.value ? 'bg-blossom-green' : 'bg-blossom-yellow'}`}
               onClick={() => setSelectedItem(custom.value)}
             >
@@ -89,21 +93,56 @@ export default function Custom(props: CustomProps) {
           );
         })}
       </div>
-      <div className="border-t border-solid border-1 border-blossom-darkGray mx-5"></div>
+      <div className="border-t border-solid border-1 border-blossom-darkGray my-[8px]"></div>
 
-      <div className="grid grid-cols-3 gap-4 pl-5 pr-3 mx-1">
-        {MOCK_IMAGES.map(img => {
-          return (
-            <div
-              key={img.id}
-              className={`w-24 ${
-                selectedItem === 'background' ? 'h-28' : 'h-14'
-              }  bg-blossom-gray border-solid border-2 border-blossom-white rounded-md`}
-              // style={{ backgroundImage: `url(${img.value})` }}
-              onClick={() => handleItemClick(img.id)}
-            ></div>
-          );
-        })}
+      <div className="grid grid-cols-3 gap-4">
+        {selectedItem === 'background' &&
+          BACKGROUND_IMAGE.map(img => {
+            return (
+              <div
+                key={img.id}
+                className={`w-[96px] h-[112px] bg-blossom-white border-solid border-2  ${
+                  selectedBackground === img.id ? ' border-blossom-green' : 'border-blossom-white'
+                }  rounded-[14px] overflow-hidden`}
+                onClick={() => handleItemClick(img.id)}
+              >
+                <Image
+                  src={`/backgrounds/${img.preview}`}
+                  alt={img.preview}
+                  width={96}
+                  height={112}
+                />
+              </div>
+            );
+          })}
+        {selectedItem === 'character' &&
+          CHARACTER_IMAGE.map(img => {
+            return (
+              <div
+                key={img.id}
+                className={`w-[96px] 'h-[56px]' bg-blossom-white border-solid border-2 ${
+                  selectedCharacter === img.id ? ' border-blossom-green' : 'border-blossom-white'
+                }   rounded-[14px] overflow-hidden`}
+                onClick={() => handleItemClick(img.id)}
+              >
+                <Image src={`/characters/${img.preview}`} alt={img.value} width={96} height={56} />
+              </div>
+            );
+          })}
+        {selectedItem === 'sticker' &&
+          STICKER_IMAGE.map(img => {
+            return (
+              <div
+                key={img.id}
+                className={`w-[96px] 'h-[56px]' bg-blossom-white border-solid border-2 ${
+                  selectedSticker === img.id ? ' border-blossom-green' : 'border-blossom-white'
+                }   rounded-[14px] overflow-hidden`}
+                onClick={() => handleItemClick(img.id)}
+              >
+                <Image src={`/stickers/${img.preview}`} alt={img.value} width={96} height={56} />
+              </div>
+            );
+          })}
       </div>
     </div>
   );
--- src/components/Creation/PageTitle/index.tsx
+++ src/components/Creation/PageTitle/index.tsx
@@ -2,7 +2,7 @@ export default function PageTitle() {
   return (
     <div className="w-full flex justify-center">
       <div
-        className={`w-60  font-title bg-blossom-lightPink border-solid border-4 border-b-8	 border-blossom-pink  text-center py-3 rounded-lg shadow-md shadow-blossom-pink`}
+        className={`w-60 bg-blossom-lightPink border-solid border-4 border-b-8 border-blossom-pink  text-center py-3 rounded-xl shadow-blossom-pink`}
       >
         나만의 벚꽃 초대장
       </div>
