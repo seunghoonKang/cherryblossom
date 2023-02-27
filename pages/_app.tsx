3acbb421d88a80c47cd7a9863a59931242c8add5
YeonghunKO <yhko1988@gmail.com>
2023-02-27 12:01:20 +0900

    feat : intro페이지에 og SEO설정
    - 미리보기 표시

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,16 @@ import '@/styles/globals.css';
 
 import type { AppProps } from 'next/app';
 
+import { DefaultSeo } from 'next-seo';
+import { DEFAULT_SEO } from '@/src/constants/defaultSEO';
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <DefaultSeo {...DEFAULT_SEO} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
