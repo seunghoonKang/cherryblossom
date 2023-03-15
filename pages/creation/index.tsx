340943ee8e786c410a46647dbb073cd22be1f845
seonghun <pch468@naver.com>
2023-03-15 21:30:40 +0900

    feat: 드래그 시 좌표보다 왼쪽 위에 캐릭터/스티커 보이게 수정

--- pages/creation/index.tsx
+++ pages/creation/index.tsx
@@ -161,13 +161,13 @@ const Creation = () => {
           <img
             src={item.path}
             alt={'dragedItem'}
-            width={48}
-            height={48}
+            width={30}
+            height={30}
             className="absolute"
             style={{
               left: `${item.offsetX}px`,
               top: `${item.offsetY}px`,
-              transform: 'translate(-50%,-50%)',
+              transform: 'translate(-100%,-100%)',
             }}
           />
         )}
