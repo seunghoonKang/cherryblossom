59df81f61f599a22079f419480670cc8721d187b
seunghoon <tmdgns4321@gmail.com>
2023-03-17 13:49:30 +0900

    fix: height 추가

--- src/components/Creation/PageTitle/index.tsx
+++ src/components/Creation/PageTitle/index.tsx
@@ -2,7 +2,7 @@ export default function PageTitle() {
   return (
     <div className="flex w-full justify-center">
       <div
-        className={`z-10 w-60	 translate-y-1/2 rounded-[10px] border-[3px] border-solid border-blossom-pink  bg-blossom-lightPink py-3 text-center text-xl shadow-blossom-pink drop-shadow-pageTitle`}
+        className={`z-10 flex h-10 w-60 translate-y-1/2 items-center justify-center rounded-[10px]  border-[3px] border-solid border-blossom-pink bg-blossom-lightPink py-3 text-center text-xl shadow-blossom-pink drop-shadow-pageTitle`}
       >
         나만의 벚꽃 초대장
       </div>
