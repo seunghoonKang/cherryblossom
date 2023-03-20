d3c4180de70d5ddb1943ce406ef54d526e14eb5a
YeonghunKO <yhko1988@gmail.com>
2023-03-20 22:23:18 +0900

    style : 토스트 메세지 스타일 피그마대로 수정
    - 폰트 pretendard로 변경
    
    - 간격 조정

--- src/components/ToastMessage/index.tsx
+++ src/components/ToastMessage/index.tsx
@@ -23,9 +23,11 @@ function ToastMessage(props: {
     <>
       {popToastMsg && (
         <div className="relative w-full">
-          <div className="absolute left-1/2 z-20 flex h-[44px] w-full max-w-[296px] translate-y-[-50%] translate-x-[-50%] items-center justify-around rounded-[6px] bg-white px-[25px]">
+          <div
+            className={` absolute left-1/2 z-20 flex h-[44px] w-[296px] translate-y-[-50%] translate-x-[-50%] items-center justify-center gap-[10px] rounded-[6px] bg-white`}
+          >
             <Image src={image} width={22} height={18} alt="img" />
-            <p className="text-[15px] leading-4">{message}</p>
+            <p className="font-pretendard text-[15px] leading-4">{message}</p>
           </div>
         </div>
       )}
